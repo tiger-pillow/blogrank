d111788299ea18aaf139d2b96f907aea1e7b6cd6
Gladys <tigerpillow@outlook.com>
2021-12-27 10:50:28 +0100

    basic stepup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Pages from './pages'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> TEST TEST TEST</h1>
-        
+        <Pages /> 
       </header>
     </div>
   );
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div> 
+      <h1>BlogRank by Tianyi & Yiyuan </h1>
+    </div>
+    
+
+  );
+};
+
+export default Header;
