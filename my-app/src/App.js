2e319c5b7f0f6e51e43bc1487be17476f5a94592
Gladys <tigerpillow@outlook.com>
2021-12-27 09:21:10 +0100

    restarted react

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> TEST TEST TEST</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
